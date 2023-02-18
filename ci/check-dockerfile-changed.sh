cfc6648f7c77cbf803f6efeb759558a661c11391
jdiestra <julius.diestra.cabrera@gmail.com>
2023-02-18 09:38:24 +0100

    Adding bash script

---
+++ ci/check-dockerfile-changed.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker_file_name='cpp-ubuntu.Dockerfile'
+flag_docker=$(git diff --name-only origin/main HEAD | grep $docker_file_name)
+if [[ "$docker_file_name" == "$flag_docker" ]]; then
+    docker_file_changed=true;
+else
+    docker_file_changed=false;
+fi
+
+echo "Docker file $docker_file_name is changed : $docker_file_changed"
